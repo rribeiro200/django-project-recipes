653c526c754a38ef7df889e1206a29efc46878ce
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-01-22 17:01:37 -0300

    create a firsts urls in the project

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def my_view(request):
+    return HttpResponse('Uma string')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view)
 ]
