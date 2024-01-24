b2d36dd90ddf74eeaa4cddb8c3b7e914d44d5030
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-01-24 11:06:38 -0300

    creating Recipe and Recipe Category models, and registering them in the Djando-Admin interface.

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from recipes.models import Category, Receipe
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Receipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Category, CategoryAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=65, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Receipe(models.Model):
+    title = models.CharField(max_length=65, null=True)
+    description = models.CharField(max_length=165, null=True)
+    slug = models.SlugField(null=True)
+    preparation_time = models.IntegerField(null=True)
+    preparation_time_unit = models.CharField(max_length=65, null=True)
+    servings = models.IntegerField(null=True)
+    servings_unit = models.CharField(max_length=65, null=True)
+    preparation_steps = models.TextField(null=True)
+    preparation_steps_is_html = models.BooleanField(default=False, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    is_published = models.BooleanField(default=False, null=True)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d', null=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
+
+    def __str__(self):
+        return self.title
