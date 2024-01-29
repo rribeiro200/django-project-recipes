d843d2ce229e90a797ed73276655d4f374ba096f
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-01-29 13:19:44 -0300

    defining pagination in HTML

--- recipes/views.py
+++ recipes/views.py
@@ -1,20 +1,37 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
+from utils.pagination import make_pagination_range
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe, Category
 from django.http import Http404, HttpResponse
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request):
     recipes = Recipe.objects.filter(
             is_published=True,
         ).order_by('-id')
+    
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+
+    paginator = Paginator(recipes, 5)
+    page_obj = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range, 
+        4,
+        current_page
+    )
 
     return render(
         request,
         'recipes/pages/home.html',
         context={
-            'recipes': recipes,
+            'recipes': page_obj,
+            'pagination_range': pagination_range
         }
     )
 
--- utils/pagination.py
+++ utils/pagination.py
@@ -1,21 +1,24 @@
-import math 
+import math
 
-def make_pagination_range(page_range, qty_pages, current_page):
+def make_pagination_range(
+    page_range,
+    qty_pages,
+    current_page,
+):
     middle_range = math.ceil(qty_pages / 2)
     start_range = current_page - middle_range
     stop_range = current_page + middle_range
     total_pages = len(page_range)
 
-    # Positivamos start_range caso seja negativo (menor que 0)
     start_range_offset = abs(start_range) if start_range < 0 else 0
 
     if start_range < 0:
         start_range = 0
-        stop_range = stop_range + start_range_offset
+        stop_range += start_range_offset
 
     if stop_range >= total_pages:
         start_range = start_range - abs(total_pages - stop_range)
-    
+
     pagination = page_range[start_range:stop_range]
     return {
         'pagination': pagination,
@@ -28,5 +31,3 @@ def make_pagination_range(page_range, qty_pages, current_page):
         'first_page_out_of_range': current_page > middle_range,
         'last_page_out_of_range': stop_range < total_pages,
     }
-
-make_pagination_range(list(range(1, 21)), 4, 1)
