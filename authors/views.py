3bea3e4c7b5211c03871b1bbacaef134c7174273
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-01-30 10:13:20 -0300

    creating new 'authors' app for users to register

---
+++ authors/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def register_view(request):
+    
+    return render(
+        request,
+        'authors/pages/register_view.html'
+    )
--- project/urls.py
+++ project/urls.py
@@ -24,6 +24,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('authors/', include('authors.urls')),
     path('', include('recipes.urls')),
 ]
 
