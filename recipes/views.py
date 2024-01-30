0c6b32ff00daa1e6ffa16daeb01f29efd663bea9
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-01-30 10:01:05 -0300

    Removing unnecessary messages

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,6 @@ from recipes.models import Recipe, Category
 from django.http import Http404, HttpResponse
 from django.db.models import Q
 from django.core.paginator import Paginator
-from django.contrib import messages
 import os
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 9))
@@ -17,10 +16,6 @@ def home(request):
         ).order_by('-id')
     
     page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
-
-    messages.error(request, 'QUE LEGAL, FOI UM SUCESSO!!!')
-    messages.success(request, 'QUE LEGAL, FOI UM SUCESSO!!!')
-    messages.info(request, 'QUE LEGAL, FOI UM SUCESSO!!!')
 
     return render(
         request,
