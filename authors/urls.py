515580822247d210f094f427e69b0009e0afe732
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-01-30 14:17:31 -0300

    Sending POST to another URL and using SESSIONS to save values between requests

--- authors/urls.py
+++ authors/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'authors'
 
 urlpatterns = [
-    path('register/', views.register_view, name='register')
+    path('register/create/', views.register_create, name='create'),
+    path('register/', views.register_view, name='register'),
 ]
--- authors/views.py
+++ authors/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import redirect, render
 from authors.forms import RegisterForm
+from pprint import pprint
 
 # Create your views here.
 def register_view(request):
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    print('Dados do formulário armazendos na sessão: ')
+    pprint(request.session['register_form_data'])
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
     
-    return render(
-        request,
-        'authors/pages/register_view.html',
+    # for field in form:
+    # ...
+
+    return render(request, 'authors/pages/register_view.html',
         {
             'form': form
         }
     )
+
+
+# View responsável somente por TRATAR os DADOS do FORMULÁRIO
+def register_create(request):
+    if not request.POST:
+        raise Http404
+
+    POST = request.POST  # QueryDict com os dados de cada campo do formulário
+    request.session['register_form_data'] = POST # Armazenando dados do formulário em uma chave da sessão.
+
+    return redirect('authors:register')
