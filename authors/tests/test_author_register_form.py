f3926ae7656fb85fe46ab4b7391c5fe0ac9744df
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-01-31 15:36:52 -0300

    unit testing and integration testing

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -1,6 +1,8 @@
-from django.test import TestCase
+from unittest import TestCase
+from django.test import TestCase as DjangoTestCase
 from authors.forms import RegisterForm
 from parameterized import parameterized
+from django.urls import reverse
 
 class AuthorRegisterFormUnitTest(TestCase):
     @parameterized.expand([
@@ -31,3 +33,40 @@ class AuthorRegisterFormUnitTest(TestCase):
         current = form[field].field.help_text
 
         self.assertEqual(current, needed)
+
+    @parameterized.expand([
+            ('username', 'Username'),
+            ('first_name', 'First name'),
+            ('last_name', 'Last name'),
+            ('email', 'E-mail'),
+            ('password', 'Password')
+    ])
+    def test_field_label(self, field, needed):
+        form = RegisterForm()
+        current = form[field].label
+
+        self.assertEqual(current, needed)
+
+
+class AuthorRegisterFormIntegrationTest(DjangoTestCase):
+    def setUp(self, *args, **kwargs):
+        self.form_data = {
+            'username': 'user',
+            'first_name': 'first',
+            'last_name': 'last',
+            'email': 'email@anyemail.com',
+            'password': 'Str0ngP@ssword1',
+            'password2': 'Str0ngP@ssword2',
+        }
+        return super().setUp(*args, **kwargs)
+    
+    
+    @parameterized.expand([
+        ('username', 'This field must not be empty'),
+    ])
+    def test_fields_cannot_be_empty(self, field, msg):
+        self.form_data[field] = ''
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        
+        self.assertEqual(msg, response.content.decode('utf-8'))
