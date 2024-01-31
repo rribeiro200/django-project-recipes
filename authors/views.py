cfdba81e1139c4d917950b606e5d45a2a6e96a97
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-01-31 11:35:08 -0300

    Saving form data to the database with the is_valid() function

--- authors/views.py
+++ authors/views.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from django.shortcuts import redirect, render
 from authors.forms import RegisterForm
 from pprint import pprint
+from django.contrib import messages
 
 # Create your views here.
 def register_view(request):
@@ -24,5 +25,12 @@ def register_create(request):
 
     POST = request.POST  # QueryDict com os dados de cada campo do formulário
     request.session['register_form_data'] = POST # Armazenando dados do formulário em uma chave da sessão.
+    form = RegisterForm(POST)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Yor user is created, please log in.')
+
+        del(request.session['register_form_data'])
 
     return redirect('authors:register')
