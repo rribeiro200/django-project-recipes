f97c71ec02854fc7e133b91510d18606b83d922c
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-02-01 17:05:53 -0300

    usando mixins para compartilhar métodos entre classes

--- recipes/tests/test_recipe_base.py
+++ recipes/tests/test_recipe_base.py
@@ -1,10 +1,7 @@
 from django.test import TestCase
 from recipes.models import Category, Recipe, User
 
-class RecipeTestBase(TestCase):
-    def setUp(self) -> None:
-        return super().setUp()
-    
+class RecipeMixin:
     def make_category(self, name='Category'):
         return Category.objects.create(name=name)
     
@@ -38,3 +35,7 @@ class RecipeTestBase(TestCase):
             preparation_steps=preparation_steps, preparation_steps_is_html=preparation_steps_is_html,
             is_published=is_published,
         )
+
+class RecipeTestBase(TestCase, RecipeMixin):
+    def setUp(self) -> None:
+        return super().setUp()
--- tests/functional_tests/recipes/base.py
+++ tests/functional_tests/recipes/base.py
@@ -1,8 +1,9 @@
 import time
 from utils.browser import make_chrome_browser
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from recipes.tests.test_recipe_base import RecipeMixin
 
-class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
+class RecipeBaseFunctionalTest(StaticLiveServerTestCase, RecipeMixin):
     def setUp(self) -> None:
         self.browser = make_chrome_browser()
         return super().setUp()
