fc70c9c1e34be1e3e256f542bf120a755c1527d0
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-02-02 16:22:32 -0300

    teste login_create aceita apenas POST

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -7,7 +7,9 @@ from selenium.webdriver.common.by import By
 @pytest.mark.functional_test
 class AuthorLoginTest(AuthorsBaseTest):
     def test_user_valid_data_can_login_successfully(self):
-        user = User.objects.create_user(username='username', password='pass')
+        # Criando um novo usuário fictício para este teste
+        string_password = 'pass'
+        user = User.objects.create_user(username='username', password=string_password)
 
         # Usuário abre a página de login
         self.browser.get(self.live_server_url + reverse('authors:login'))
@@ -18,8 +20,8 @@ class AuthorLoginTest(AuthorsBaseTest):
         password_field = self.get_by_placeholder(form, 'Type your password')
 
         # Logando o usuário
-        username_field.send_keys('username')
-        password_field.send_keys('pass')
+        username_field.send_keys(user.username)
+        password_field.send_keys(string_password)
         
         # Submetendo o form
         form.submit()
@@ -29,3 +31,10 @@ class AuthorLoginTest(AuthorsBaseTest):
 
         # Fim do teste
         self.assertIn('Login successfully!', body)
+
+    def test_login_create_raises_404_if_not_POST_method(self):
+        url = self.browser.get(self.live_server_url + reverse('authors:login_create'))
+
+        body = self.browser.find_element(By.TAG_NAME, 'body').text
+
+        self.assertIn('Not found', body)
