787f70ad194619c600e40d8c5a54f9fe28b7a5fc
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-02-08 17:29:55 -0300

    solucionando erro global do Django com rederização do estilo para navegadores, adicionando mimetypes no settings do project

--- project/settings.py
+++ project/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from django.contrib.messages import constants
 from pathlib import Path
 import os
+import mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
