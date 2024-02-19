fe0c25beda8db36197cb9ea2c1ef5cf08d175cd1
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-02-19 14:27:04 -0300

    Usando JS na lógica do menu

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -1,4 +1,4 @@
-function my_scope(){
+(() => {
     // Selecionando forms pela classe
     const forms = document.querySelectorAll('.form-delete');
     
@@ -14,5 +14,51 @@ function my_scope(){
             }
         })
     }
-}
-my_scope();
+})();
+
+
+// Lógica do menu
+(() => {
+    const buttonCloseMenu = document.querySelector('.button-close-menu');
+    const buttonShowMenu = document.querySelector('.button-show-menu');
+    const menuContainer = document.querySelector('.menu-container')
+    
+    const buttonShowMenuVisibleClass = 'button-show-menu-visible';
+    const menuHiddenClass = 'menu-hidden';
+
+    const closeMenu = () => {
+        buttonShowMenu.classList.add(buttonShowMenuVisibleClass);
+        menuContainer.classList.add(menuHiddenClass);
+    }
+
+    const showMenu = () => {
+        buttonShowMenu.classList.remove(buttonShowMenuVisibleClass)
+        menuContainer.classList.remove(menuHiddenClass)
+    }
+
+    if (buttonCloseMenu){
+        buttonCloseMenu.removeEventListener('click', closeMenu)
+        buttonCloseMenu.addEventListener('click', closeMenu);
+    }
+
+    if (buttonShowMenu){
+        buttonShowMenu.removeEventListener('click', showMenu)
+        buttonShowMenu.addEventListener('click', showMenu)
+    }
+
+})();
+
+
+// Lógica do logout 
+(() => {
+    const authorsLogoutLinks = document.querySelectorAll('.authors-logout-link')
+    const formLogout = document.querySelector('.form_logout')
+
+    for (const link of authorsLogoutLinks){
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            
+            formLogout.submit()
+        })
+    }
+})();
