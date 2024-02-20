99887046b444701d110d7f27ca46414c8fdd6071
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-02-20 11:13:23 -0300

    Apagando imagens antigas de maneira condicional caso a recipe seja atualizada

--- recipes/signals.py
+++ recipes/signals.py
@@ -1,15 +1,27 @@
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 from recipes.models import Recipe
 import os
 
+# Apaga caminho do cover
 def delete_cover(instance):
     try:
         os.remove(instance.cover.path)
     except (ValueError, FileNotFoundError) as e:
         ...
 
+# Apagando um cover do diretório da aplicação
 @receiver(pre_delete, sender=Recipe)
 def recipe_cover_delete(sender, instance, *args, **kwargs):
     old_instance = Recipe.my_manager.get(pk=instance.pk)  # Instância pré-deletada
     delete_cover(old_instance)
+
+
+# Depois de atualizar o cover, apagando o caminho do antigo cover.
+@receiver(pre_save, sender=Recipe)
+def recipe_cover_update(sender, created, instance, *args, **kwargs):
+    old_instance = Recipe.my_manager.get(pk=instance.pk)
+    is_new_cover = old_instance.cover != instance.cover
+
+    if is_new_cover:
+        delete_cover(old_instance)
