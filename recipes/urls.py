5fcbcc73fee07d0ce5b944f49871d1495ce4baae
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-02-20 14:33:35 -0300

    Utilizando serializer para list e detail view com Django Rest Framework

--- recipes/urls.py
+++ recipes/urls.py
@@ -9,12 +9,14 @@ urlpatterns = [
     path('recipes/search/', site.RecipeListViewSearch.as_view(), name='search'),  # type: ignore
     path('recipes/category/<int:category_id>/', site.RecipeListViewCategory.as_view(), name='category'),
     path('recipes/<int:pk>/', site.RecipeDetailViewBase.as_view(), name='recipe'),
-    # API Test
+    # API - V1
     path('recipes/api/v1', site.RecipeListViewHomeApi.as_view(), name='recipe_api'),
     path('recipes/api/v1/<int:pk>', site.RecipeDetailAPI.as_view(), name='recipe_api_v1_detail'),
     # Teoria
     path('recipes/theory/', site.theory, name='theory'),
     # Tag
     path('recipes/tags/<slug:slug>', site.RecipeListViewTag.as_view(), name='tags'),
-    path('recipes/api/v2/', api.recipe_api_list, name='recipe_api_v2')
+    # API - V2
+    path('recipes/api/v2/', api.recipe_api_list, name='recipe_api_v2'),
+    path('recipes/api/v2/<int:pk>/', api.recipe_api_detail, name='recipe_api_v2_detail'),
 ]
--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,10 +1,35 @@
 # Django
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 # Rest Framework
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
+# Models
+from ..models import Recipe
+from ..serializers import RecipeSerializer
+
+
+# Visualização de lista
 @api_view()
 def recipe_api_list(request):
-    return Response('Oi')
+    recipes = Recipe.my_manager.get_published()[:10]
+    serializer = RecipeSerializer(instance=recipes, many=True)
+
+    return Response(serializer.data)
+
+
+# Visualização de detalhe
+@api_view()
+def recipe_api_detail(request, pk):
+    recipe = Recipe.my_manager.filter(pk=pk).first()
+
+    if recipe:
+        serializer = RecipeSerializer(instance=recipe, many=False)
+        return Response(serializer.data)
+    else:
+        return Response({
+            'detail': 'Eita! Nenhum objeto encontrado.'
+        }, status=status.HTTP_418_IM_A_TEAPOT)
