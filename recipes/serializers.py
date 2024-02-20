28b5e33897e437fefc10913a53e4506215d93950
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-02-20 15:16:02 -0300

    Renomeando campos e usando métodos de resposta no serializer do DRF

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -6,3 +6,8 @@ class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=65)
     description = serializers.CharField(max_length=165)
+    public = serializers.BooleanField(source='is_published')
+    preparation = serializers.SerializerMethodField(method_name='get_preparation')
+
+    def get_preparation(self, recipe):
+        return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
--- recipes/urls.py
+++ recipes/urls.py
@@ -9,13 +9,17 @@ urlpatterns = [
     path('recipes/search/', site.RecipeListViewSearch.as_view(), name='search'),  # type: ignore
     path('recipes/category/<int:category_id>/', site.RecipeListViewCategory.as_view(), name='category'),
     path('recipes/<int:pk>/', site.RecipeDetailViewBase.as_view(), name='recipe'),
+    
     # API - V1
     path('recipes/api/v1', site.RecipeListViewHomeApi.as_view(), name='recipe_api'),
     path('recipes/api/v1/<int:pk>', site.RecipeDetailAPI.as_view(), name='recipe_api_v1_detail'),
+    
     # Teoria
     path('recipes/theory/', site.theory, name='theory'),
+    
     # Tag
     path('recipes/tags/<slug:slug>', site.RecipeListViewTag.as_view(), name='tags'),
+    
     # API - V2
     path('recipes/api/v2/', api.recipe_api_list, name='recipe_api_v2'),
     path('recipes/api/v2/<int:pk>/', api.recipe_api_detail, name='recipe_api_v2_detail'),
