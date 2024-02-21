868e25a8babc8fd0698aca42bd9cf8b3b20e1806
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-02-21 13:30:57 -0300

    Usando RetrieveUpdateDestroyAPIView e sobrescrevendo métodos para customização

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -7,7 +7,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView, ListCreateAPIView
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import PageNumberPagination
 
 # Models
@@ -29,41 +29,10 @@ class RecipeAPIV2List(ListCreateAPIView):
 
 
 # Detalhes
-class RecipeAPIV2Detail(APIView):
-    def get_recipe(self, pk):
-        recipe = get_object_or_404(Recipe.my_manager.get_published(), pk=pk)
-        
-        return recipe
-
-    def get(self, request, pk):
-        recipe = self.get_recipe(pk=pk)
-        serializer = RecipeSerializer(
-            instance=recipe,
-            many=False,
-            context={'request': request}
-        )
-
-        return Response(serializer.data)
-    
-    def patch(self, request, pk):
-        recipe = self.get_recipe(pk=pk)
-        serializer = RecipeSerializer(
-            instance=recipe,
-            data=request.data,
-            many=False,
-            context={'request': request},
-            partial=True
-        )
-        serializer.is_valid()
-        serializer.save()
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def delete(self, request, pk):
-        recipe = self.get_recipe(pk=pk)
-        recipe.delete()
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class RecipeAPIV2Detail(RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.my_manager.get_published()
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIV2Pagination
 
 
 # Detalhes
