84b972d785ad0d24f4bd7cab893a1ddb67e51cb0
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-02-21 17:12:50 -0300

    Criando permissões no DRF

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,16 +1,14 @@
 # Django
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from ..permissions import IsOwner
 
 # Rest Framework
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Models
 from ..models import Recipe
@@ -28,7 +26,39 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.my_manager.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIV2Pagination
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_object(self):
+        pk = self.kwargs.get('pk', '')
+        obj = get_object_or_404(
+            self.get_queryset(),
+            pk=pk
+        )
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+    
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsOwner(), ]
+
+        return super().get_permissions()
+    
+    def partial_update(self, request, *args, **kwargs):
+        recipe = self.get_object()
+        serializer = RecipeSerializer(
+            instance=recipe,
+            data=request.data,
+            many=False,
+            context={'request': request},
+            partial=True
+        )
+        serializer.is_valid()
+        serializer.save()
+
+        return Response(serializer.data)
+    
 
 # Detalhes
 class TagAPIV2Detail(APIView):
