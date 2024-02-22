ee1ffb3c59e4dc339b90ff53ee86de1949ee1c43
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-02-22 12:01:12 -0300

    Criando primeiro teste utilizando self.client.get(GET) para testar se a API retorna o código de 200 ok

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -1,6 +1,19 @@
+# Rest Framework
 from rest_framework import test
+# Django 
+from django.urls import reverse
+# Recipes
 from recipes.tests.test_recipe_base import RecipeMixin
 
+
+# Classe de teste da API das receitas
 class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
+    def test_recipe_api_list_returns_status_code_200(self):
+        api_url = reverse('recipes:recipes-api-list')
+        response = self.client.get(api_url)
+
+        self.assertEqual(response.status_code, 200)
+
+
     def test_(self):
-        
+        ... 
