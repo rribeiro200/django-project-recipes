9a65be4a9aafd0d9b5113b785c4557c044907164
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-02-22 10:37:49 -0300

    Criando um método especial na ViewSet e também uma URL personalizada no router dessa ViewSet

--- authors/views/api.py
+++ authors/views/api.py
@@ -4,6 +4,7 @@ from recipes.permissions import IsOwner
 
 # Rest Framework
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.permissions import IsAuthenticated
@@ -19,6 +20,16 @@ class AuthorViewSet(ReadOnlyModelViewSet):
     permission_classes = [IsAuthenticated, ]
 
     def get_queryset(self):
-        qs = User.objects.filter(username=self.request.user.username)
+        qs = User.objects.filter(username=self.request.user.username) # type: ignore
         
         return qs
+    
+
+    @action(methods=['get'], detail=False)
+    def me(self, request, *args, **kwargs):
+        obj = self.get_queryset().first()
+        serializer = self.get_serializer(
+            instance=obj,
+        )
+
+        return Response(serializer.data)
