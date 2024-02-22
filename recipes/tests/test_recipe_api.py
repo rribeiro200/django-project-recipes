1cc8af8e5ec392f515ee3d8e0d13f110da521564
Rafael Ribeiro <rafaelribdev@gmail.com>
2024-02-22 15:12:32 -0300

    Testando se o usuário enviou o JWT Token para fazer POST no DRF

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -1,38 +1,99 @@
-# Rest Framework
-from rest_framework import test
-# Django 
-from django.urls import reverse
-# Recipes
-from recipes.tests.test_recipe_base import RecipeMixin
 # Unittest
 from unittest.mock import patch
+# Django
+from django.urls import reverse
+# Rest Framework
+from rest_framework import test
+# Mixin
+from recipes.tests.test_recipe_base import RecipeMixin
 
 
-# Classe de teste da API das receitas
 class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
+    def get_recipe_reverse_url(self, reverse_result=None):
+        api_url = reverse_result or reverse('recipes:recipes-api-list')
+
+        return api_url
+
+
+    def get_recipe_api_list(self, reverse_result=None):
+        response = self.client.get(self.get_recipe_reverse_url(reverse_result))
+
+        return response
+
+
     def test_recipe_api_list_returns_status_code_200(self):
-        api_url = reverse('recipes:recipes-api-list')
-        response = self.client.get(api_url)
-
-        self.assertEqual(response.status_code, 200)
-
-
-    @patch('recipes.views.api.RecipeAPIV2Pagination.page_size', new=5)
-    def test_recipe_api_list_loads_correct_number_of_recipes(self):
-        # AJUSTE - Dados que eu quero
-        wanted_number_of_recipes = 5
-        self.make_recipe_in_batch(wanted_number_of_recipes)
-
-        # AÇÃO
-        response = self.client.get(reverse('recipes:recipes-api-list'))
-        qtd_recipes_received = response.data.get('results') # type: ignore
-
-        # ASSERÇÃO - Verdade = teste passa, Mentira = teste falha.
+        response = self.get_recipe_api_list()
         self.assertEqual(
-            wanted_number_of_recipes,
-            qtd_recipes_received
+            response.status_code,
+            200
         )
 
 
-    def test_(self):
-        ...
+    @patch('recipes.views.api.RecipeAPIV2Pagination.page_size', new=7)
+    def test_recipe_api_list_loads_correct_number_of_recipes(self):
+        wanted_number_of_recipes = 7
+        self.make_recipe_in_batch(qtd=wanted_number_of_recipes)
+
+        response = self.client.get(
+            reverse('recipes:recipes-api-list') + '?page=1'
+        )
+        qtd_of_loaded_recipes = len(response.data.get('results'))
+
+        self.assertEqual(
+            wanted_number_of_recipes,
+            qtd_of_loaded_recipes
+        )
+
+
+    def test_recipe_api_list_do_not_show_not_published_recipes(self):
+        recipes = self.make_recipe_in_batch(qtd=2)
+        recipe_not_published = recipes[0]
+        recipe_not_published.is_published = False
+        recipe_not_published.save()
+        response = self.get_recipe_api_list()
+        self.assertEqual(
+            len(response.data.get('results')),
+            1
+        )
+
+
+    @patch('recipes.views.api.RecipeAPIV2Pagination.page_size', new=10)
+    def test_recipe_api_list_loads_recipes_by_category_id(self):
+        # Creates categories
+        category_wanted = self.make_category(name='WANTED_CATEGORY')
+        category_not_wanted = self.make_category(name='NOT_WANTED_CATEGORY')
+
+        # Creates 10 recipes
+        recipes = self.make_recipe_in_batch(qtd=10)
+
+        # Change all recipes to the wanted category
+        for recipe in recipes:
+            recipe.category = category_wanted
+            recipe.save()
+
+        # Change one recipe to the NOT wanted category
+        # As a result, this recipe should NOT show in the page
+        recipes[0].category = category_not_wanted
+        recipes[0].save()
+
+        # Action: get recipes by wanted category_id
+        api_url = reverse('recipes:recipes-api-list') + f'?category_id={category_wanted.id}' # type: ignore
+        response = self.get_recipe_api_list(reverse_result=api_url)
+
+        # We should only see recipes from the wanted category
+        self.assertEqual(
+            len(response.data.get('results')), # type: ignore
+            9
+        )
+
+
+    def test_recipe_api_list_user_must_send_jwt_token_to_create_recipe(self):
+        # Ação
+        api_url = self.get_recipe_reverse_url()
+        response = self.client.post(api_url)
+
+        # Asserção
+        self.assertEqual(
+            response.status_code,
+            401
+        )
